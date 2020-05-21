c6d1e55f7ccb71d062e4acba35d37d1644d11b9c
Basit Munir <basitmunir.pk@gmail.com>
2020-05-21 05:38:11 +0500

    theme skin and width handling

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isLightTheme;
+  isFullWidth;
+  language;
 
-  constructor() { }
+  constructor( private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    this.switchSkinColor();
+    this.switchContainerWidth();
+  }
+
+  setTheme ($event, lightTheme=false) {
+    $event.preventDefault();
+    localStorage.setItem('theme', lightTheme ? 'light' : 'dark');
+    this.switchSkinColor();
+  }
+
+  setWidth ($event, fullWidth=false) {
+    $event.preventDefault();
+    localStorage.setItem('container_width', fullWidth ? 'full' : 'no');
+    this.switchContainerWidth();
+  }
+
+  setLocale ($event, language = 'en') {
+    $event.preventDefault();
+    localStorage.setItem('language', language);
+    this.switchLanguage();
+  }
+
+  switchLanguage () {
+    this.language = localStorage.getItem('language');
+  }
+
+  switchContainerWidth () {
+    this.isFullWidth = localStorage.getItem('container_width') || 'no';
+    this.renderer.removeClass(document.body, 'menu-fullwidth')
+    if(this.isFullWidth === 'full') {
+      this.renderer.addClass(document.body, 'menu-fullwidth');
+    }
+  }
+
+  switchSkinColor () {
+    this.isLightTheme = localStorage.getItem('theme');
+    this.renderer.removeClass(document.body, 'theme-light')
+    if(this.isLightTheme === 'light') {
+      this.renderer.addClass(document.body, 'theme-light');
+    }
   }
 
 }
