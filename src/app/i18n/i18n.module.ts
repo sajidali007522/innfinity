c2a3c55cf59a25bc20ab6db7b7af267e0753bb23
Basit Munir <basitmunir.pk@gmail.com>
2020-05-21 08:40:47 +0500

    resources to support multilingual

---
+++ src/app/i18n/i18n.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateCacheModule, TranslateCacheSettings, TranslateCacheService } from 'ngx-translate-cache';
+
+@NgModule({
+  imports: [
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    TranslateCacheModule.forRoot({
+      cacheService: {
+        provide: TranslateCacheService,
+        useFactory: translateCacheFactory,
+        deps: [TranslateService, TranslateCacheSettings]
+      },
+      cacheMechanism: 'Cookie'
+    })
+  ],
+  exports: [TranslateModule]
+})
+export class I18nModule {
+  constructor(
+    translate: TranslateService,
+    translateCacheService: TranslateCacheService
+  ) {
+    translateCacheService.init();
+    translate.addLangs(['en', 'es', 'fr']);
+    const browserLang = translateCacheService.getCachedLanguage() || translate.getBrowserLang();
+    translate.use(browserLang.match(/en|es|fr/) ? browserLang : 'en');
+  }
+}
+
+export function translateCacheFactory(
+  translateService: TranslateService,
+  translateCacheSettings: TranslateCacheSettings
+) {
+  return new TranslateCacheService(translateService, translateCacheSettings);
+}
+
+export function translateLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
