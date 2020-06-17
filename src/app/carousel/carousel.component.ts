9511fbc98e7ab763b47ef5b653b7045d11566853
Basit Munir <basitmunir.pk@gmail.com>
2020-06-18 02:38:50 +0500

    carousel integrated into system

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  constructor() { }
+  constructor (private renderer: Renderer2) {
+    this.addJsToElement('assets/carousel/assets/js/slick.min.js');
+    this.addJsToElement('assets/carousel/assets/js/hp.js');
+  }
 
   ngOnInit(): void {
+  }
+
+  addJsToElement(src: string): HTMLScriptElement {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    this.renderer.appendChild(document.body, script);
+    return script;
   }
 
 }
