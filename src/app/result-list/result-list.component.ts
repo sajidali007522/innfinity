1df423626fd47c36a6547037cdd3799f1813bc4d
Basit Munir <basitmunir.pk@gmail.com>
2020-08-13 17:28:35 +0500

    fix

--- src/app/result-list/result-list.component.ts
+++ src/app/result-list/result-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Options} from "ng5-slider";
 
 @Component({
   selector: 'app-result-list',
@@ -6,7 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./result-list.component.css']
 })
 export class ResultListComponent implements OnInit {
-
+  value: number = 40;
+  highValue: number = 60;
+  options: Options = {
+    floor: 0,
+    ceil: 100
+  };
+  state= {
+    price : {
+      value: 40,
+      highValue: 60,
+      options: {
+        floor: 0,
+        ceil: 100
+      }
+    },
+    arrival: {
+      value: 12,
+      highValue: 18,
+      options: {
+        floor: 0,
+        ceil: 24
+      }
+    },
+    departure: {
+      value: 12,
+      highValue: 18,
+      options: {
+        floor: 0,
+        ceil: 24
+      }
+    },
+    stops: {
+      value: 12,
+      highValue: 18,
+      options: {
+        floor: 0,
+        ceil: 24
+      }
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
